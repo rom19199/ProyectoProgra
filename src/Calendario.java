79d9b6defb93f51f625a30f0ee0ce4ee672a763c
Sebasssiu <54748964+Sebasssiu@users.noreply.github.com>
2019-09-04 20:29:49 -0600

    Update Calendario.java

--- src/Calendario.java
+++ src/Calendario.java
@@ -1,4 +1,106 @@
 
 public class Calendario {
+	private String Usuario;
+	private String Password;
+	private String Nombre;
+	private int Edad;
+	private int EstaturaCentimetros;
+	private float PesoKilogramos;
+	private String ActividadFisica;
+	public Calendario () {
+		Usuario = "";
+		Password = "";
+		Nombre = "";
+		Edad = 0;
+		EstaturaCentimetros = 0;
+		PesoKilogramos = 0;
+		ActividadFisica = "";
+		
+	}
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return Usuario;
+	}
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		Usuario = usuario;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return Password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		Password = password;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return Nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return Edad;
+	}
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		Edad = edad;
+	}
+	/**
+	 * @return the estaturaCentimetros
+	 */
+	public int getEstaturaCentimetros() {
+		return EstaturaCentimetros;
+	}
+	/**
+	 * @param estaturaCentimetros the estaturaCentimetros to set
+	 */
+	public void setEstaturaCentimetros(int estaturaCentimetros) {
+		EstaturaCentimetros = estaturaCentimetros;
+	}
+	/**
+	 * @return the pesoKilogramos
+	 */
+	public float getPesoKilogramos() {
+		return PesoKilogramos;
+	}
+	/**
+	 * @param pesoKilogramos the pesoKilogramos to set
+	 */
+	public void setPesoKilogramos(float pesoKilogramos) {
+		PesoKilogramos = pesoKilogramos;
+	}
+	/**
+	 * @return the actividadFisica
+	 */
+	public String getActividadFisica() {
+		return ActividadFisica;
+	}
+	/**
+	 * @param actividadFisica the actividadFisica to set
+	 */
+	public void setActividadFisica(String actividadFisica) {
+		ActividadFisica = actividadFisica;
+	}
+	
 
 }
