d1b14ff1c8298cdfce7a1cd672901a236d944d1c
Sebasssiu <54748964+Sebasssiu@users.noreply.github.com>
2019-09-04 20:42:21 -0600

    Update Operaciones.java

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,1 +1,15 @@
-
+public class Operaciones {
+  private float MIC;
+  private String complexion;
+  public Operaciones (){
+    MIC = 0;
+    complexion = "";
+  }
+  Public float encontrarMIC (Calendario persona) {
+    MIC = getPesoKilogramos() / getEstaturaCentimetros();
+    return MIC;
+  }
+  public String encontrarComplexion () {
+    if (MIC > 25) {
+      complexion
+}
